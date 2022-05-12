0d5809d83fc5404e9a793c41603baea65de1e478
Ariful-Hasan <arifhasan.mbstu@gmail.com>
2022-05-12 13:36:07 +0200

    router added

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -4,8 +4,20 @@ require_once __DIR__ . '../../vendor/autoload.php';
 
 use App\Core\App;
 use App\Core\Config;
+use App\Route\Router;
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
 new App(new Config($_ENV));
+
+
+/**
+ * register all routes
+ */
+$router = new Router();
+
+$router
+    ->get('/', [App\Controllers\HomeController::class, 'index']);
+
+$router->resolve($_SERVER['REQUEST_URI'], strtolower($_SERVER['REQUEST_METHOD']));
